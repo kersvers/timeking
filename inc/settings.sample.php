2fcdd128a0c0f9f7e14925fd40ec2d9895d42056
Rene Sijnke <rene@krsvrs.nl>
2012-09-03 23:39:12 +0200

    added less files and changed config sample file

--- inc/settings.sample.php
+++ inc/settings.sample.php
@@ -6,20 +6,11 @@ $config["harvest_pass"]     = 'password'; // Your Harvest password
 $config["harvest_account"]  = 'username'; // The {myaccount} portion of your Harvest url: {myaccount}.harvestapp.com
 
 // number of working hours a day you want to measure against
-$config["working_hours_per_day"]["default"] = 7.5;
-$config["working_hours_per_day"]["rasmus@reload.dk"] = 7.5;
-$config["working_hours_per_day"]["kasper@reload.dk"] = 7.5;
-$config["working_hours_per_day"]["thomas@reload.dk"] = 7.5;
-$config["working_hours_per_day"]["martin@reload.dk"] = 4;
-$config["working_hours_per_day"]["hasse@reload.dk"] = 7.5;
-$config["working_hours_per_day"]["troels@reload.dk"] = 7.5;
-$config["working_hours_per_day"]["christoffer@reload.dk"] = 4.8;
-$config["working_hours_per_day"]["arne@reload.dk"] = 7.5;
-$config["working_hours_per_day"]["fini@reload.dk"] = 7.5;
-$config["working_hours_per_day"]["esben@reload.dk"] = 7.5;
+$config["working_hours_per_day"]["default"] = 5;
+$config["working_hours_per_day"]["user@company.com"] = 5;
 
 // site title
-$config["site_title"]       = 'Reloads Timetable King';
+$config["site_title"]       = 'Kersvers Timetable King';
 
 // wanna track with Google Analytics? Enter your Account ID
 $config["ga_account_id"]    = '';
